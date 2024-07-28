21ec5c6fb813170998063a5bc9e528fb17c566b6
niv vaknin <niv@certora.com>
2024-07-28 20:25:55 +0300

    Fix setup py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=read_requirements(),
     entry_points={
         "console_scripts": [
-            "proposaltools=ProposalTools.main:main",  # Replace with the actual path to your main function
+            "CheckProposal=ProposalTools.main:main",  # Replace with the actual path to your main function
         ],
     },
 )
