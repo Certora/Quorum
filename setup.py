30f478e7be588832a11f297693b08ebb899bc67e
niv vaknin <niv@certora.com>
2024-07-28 20:35:41 +0300

    Try to fix setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read_requirements():
         return f.read().splitlines()
 
 setup(
-    name="ProposalsTool",
+    name="ProposalTools",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
