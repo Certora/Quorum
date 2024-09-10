f186f4fa7e2f3ff03deea9f9e9d7448058e9515f
Niv vaknin <122722245+nivcertora@users.noreply.github.com>
2024-09-10 16:53:29 +0300

    Delete ProposalTools/Checks directory

--- ProposalTools/Checks/__init__.py
+++
@@ -1,6 +0,0 @@
-from .diff import DiffCheck
-from .global_variables import GlobalVariableCheck
-from .feed_price import FeedPriceCheck
-from .new_listing import NewListingCheck
-
-all = ["DiffCheck", "GlobalVariableCheck", "FeedPriceCheck", "NewListingCheck"]
