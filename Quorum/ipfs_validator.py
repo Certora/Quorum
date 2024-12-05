68ec77060510a6a5e96cd9771eba7a4aabec0c42
liav-certora <liav@certora.com>
2024-12-05 12:17:14 +0200

    Niv CR

--- Quorum/ipfs_validator.py
+++ Quorum/ipfs_validator.py
@@ -1,4 +1,5 @@
 from Quorum.utils.chain_enum import Chain
+import Quorum.utils.arg_validations as arg_valid
 import Quorum.config as config
 from Quorum.apis.block_explorers.chains_api import ChainAPI
 from Quorum.apis.llms.claude_conversation import ClaudeConversation
@@ -9,17 +10,19 @@ import re
 from pathlib import Path
 
 
+IPFS_CACHE = Path(__file__).parent / '.ipfs_cache'
+IPFS_CACHE.mkdir(exist_ok=True)
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Compare ipfs with actual payload.')
     parser.add_argument('--proposal_id', type=int, help='The id of the proposal.')
     parser.add_argument('--chain', type=str, choices=[chain.value for chain in Chain], help='Blockchain chain.')
-    parser.add_argument('--proposal_address', type=str, help='Ethereum proposal address.')
-    parser.add_argument('--prompt_template', type=Path, default=Path(__file__).parent / 'ipfs_validation_prompt.txt',
+    parser.add_argument('--proposal_address', type=arg_valid.validate_address, help='Ethereum proposal address.')
+    parser.add_argument('--prompt_template', type=arg_valid.validate_path, default=Path(__file__).parent / 'ipfs_validation_prompt.txt',
                         help='Optional other template for prompting the LLM.')
     
     args = parser.parse_args()
-    if not args.prompt_template.exists():
-        raise argparse.ArgumentError(f'Could not find prompts at {args.prompt_template}.')
 
     return args
 
@@ -29,6 +32,11 @@ def parse_args() -> argparse.Namespace:
 #       We should remove this function once we implement a better way to get the data using an API that 
 #       Aave is currently working on or their "v3-governance-cache" repo. This should be done on a different PR.
 def get_raw_ipfs(proposal_id: int) -> str:
+    cache = IPFS_CACHE / f'{proposal_id}.txt'
+    if cache.exists():
+        with open(cache) as f:
+            return f.read()
+    
     resp = requests.get(f'https://vote.onaave.com/proposal/?proposalId={proposal_id}')
     resp.raise_for_status()
     voting_ui_html = resp.content.decode()
@@ -45,7 +53,11 @@ def get_raw_ipfs(proposal_id: int) -> str:
     
     resp = requests.get(f'https://ipfs.io/ipfs/{ipfs_hash}')
     resp.raise_for_status()
-    return resp.content.decode()
+    
+    content = resp.content.decode()
+    with open(cache, 'w') as f:
+        f.write(content)
+    return content
 
 
 def main():
@@ -55,7 +67,7 @@ def main():
     source_codes = block_explorer.get_source_code(args.proposal_address)
     payload = '\n'.join(source_codes[0].file_content)
 
-    ipfs = get_raw_ipfs(160)
+    ipfs = get_raw_ipfs(args.proposal_id)
 
     with open(args.prompt_template) as f:
         all_prompt_templates = f.read()
