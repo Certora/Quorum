72c247e0f1f6ebf7c4462f2e1f16a901c808d534
niv vaknin <niv@certora.com>
2025-01-06 14:10:06 +0200

    Fix test

--- Quorum/tests/test_auto_report.py
+++ Quorum/tests/test_auto_report.py
@@ -1,8 +1,9 @@
 import pytest
+import argparse
 
 import Quorum.tests.conftest as conftest
 
-import Quorum.entry_points.create_report as create_report
+import Quorum.entry_points.implementations.create_report as create_report
 
 from pathlib import Path
 
@@ -13,8 +14,14 @@ EXPECTED_DIR = conftest.EXPECTED_DIR / 'test_auto_report'
 def test_auto_report():
     with open(EXPECTED_DIR / 'v3-132.md') as f:
         expected = f.read()
+
+    args = argparse.Namespace(
+        proposal_id=132,
+        template=Path('Quorum') / 'auto_report' / 'AaveReportTemplate.md.j2',
+        generate_report_path=None
+    )
     
-    create_report.run_create_report(132, create_report.DEFAULT_TEMPLATE_PATH, Path('v3-132.md'))
+    create_report.run_create_report(args)
     with open(Path('v3-132.md')) as f:
         actual = f.read()
     
