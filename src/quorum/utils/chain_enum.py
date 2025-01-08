8a02070c3f4ef835582bdf8367a25b77e0f1f65f
niv vaknin <niv@certora.com>
2025-01-08 22:01:52 +0200

    Fix linter in CI

--- src/quorum/utils/chain_enum.py
+++ src/quorum/utils/chain_enum.py
@@ -5,15 +5,16 @@ class Chain(StrEnum):
     """
     Enumeration for supported blockchain networks.
     """
-    ETH = 'Ethereum'
-    ARB = 'Arbitrum'
-    AVAX = 'Avalanche'
-    BASE = 'Base'
-    BSC = 'BNBChain'
-    GNO = 'Gnosis'
-    MET = 'Metis'
-    OPT = 'Optimism'
-    POLY = 'Polygon'
-    SCROLL = 'Scroll'
-    ZK = 'zkSync'
-    LINEA= 'LINEA'
+
+    ETH = "Ethereum"
+    ARB = "Arbitrum"
+    AVAX = "Avalanche"
+    BASE = "Base"
+    BSC = "BNBChain"
+    GNO = "Gnosis"
+    MET = "Metis"
+    OPT = "Optimism"
+    POLY = "Polygon"
+    SCROLL = "Scroll"
+    ZK = "zkSync"
+    LINEA = "LINEA"
