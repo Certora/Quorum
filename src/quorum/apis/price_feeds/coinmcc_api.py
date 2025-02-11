7ddc23d6fbcfc1cb641f3489aaa2fb8ef377a70b
Niv vaknin <122722245+nivcertora@users.noreply.github.com>
2025-02-11 12:40:09 +0200

    CERT 8126 Support CMC price feed api provider (#82)
    * CERT 8126 Support CMC price feed api provider
    
    * Update README.md
    
    * Update CI
    
    * Update src/quorum/apis/price_feeds/coinmcc_api.py
    
    Co-authored-by: yoav-el-certora <122207807+yoav-el-certora@users.noreply.github.com>
    
    * CR comments
    
    * Add comment
    
    * Platform check fix
    
    ---------
    
    Co-authored-by: yoav-el-certora <122207807+yoav-el-certora@users.noreply.github.com>

---
+++ src/quorum/apis/price_feeds/coinmcc_api.py
@@ -0,0 +1,73 @@
+import os
+
+from quorum.utils.chain_enum import Chain
+from quorum.utils.singleton import singleton
+
+from .price_feed_utils import PriceFeedData, PriceFeedProvider, PriceFeedProviderBase
+
+
+@singleton
+class CoinMarketCapAPI(PriceFeedProviderBase):
+    """
+    CoinMarketCapAPI is a class designed to interact with the CoinMarketCap API.
+    It fetches and stores price feed data for various blockchain networks supported by CoinMarketCap.
+    """
+
+    COINMARKETCAP_API_URL = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/info"
+
+    def __init__(self):
+        super().__init__()
+        self.api_key = os.getenv("COINMARKETCAP_API_KEY")
+        if not self.api_key:
+            raise ValueError(
+                "CoinMarketCap API key not found in environment variables.\n"
+                "Please set COINMARKETCAP_API_KEY if you want to use CoinMarketCap API as a price feed provider,\n"
+                "or remove CoinMarketCapAPI from the list of price feed providers."
+            )
+
+    def _get_price_feed_info(self, chain: Chain, address: str) -> PriceFeedData | None:
+        """
+        Get price feed data for a given address on a blockchain network.
+
+        Args:
+            chain (Chain): The blockchain network to fetch price feeds for.
+            address (str): The contract address of the price feed.
+
+        Returns:
+            PriceFeedData: The price feed data for the specified address, or None if not found.
+        """
+        parameters = {
+            "CMC_PRO_API_KEY": self.api_key,
+            "address": address,
+        }
+        response = self.session.get(self.COINMARKETCAP_API_URL, params=parameters)
+        if not response.ok:
+            return None
+
+        raw_data: dict = response.json()
+        # Extract the first value from the "data" mapping
+        price_data = next(iter(raw_data.get("data", {}).values()))
+        if not price_data:
+            return None
+
+        # Check if platform data exists and the platform name matches the provided chain (case-insensitive)
+        if (
+            not price_data.get("platform")
+            or not price_data["platform"].get("name")
+            or price_data["platform"]["name"].lower() != chain.value.lower()
+        ):
+            return None
+
+        # Update the data with the token address from the platform info
+        token_address = price_data["platform"]["token_address"]
+        # Add the proxy address to the price feed data if available
+        if token_address:
+            price_data.update({"proxy_address": token_address})
+        price_data.update({"address": address})
+        return PriceFeedData(**price_data)
+
+    def get_name(self) -> PriceFeedProvider:
+        """
+        Return the provider name for identification.
+        """
+        return PriceFeedProvider.COINMARKETCAP
--- src/quorum/tests/test_price_feed_providers.py
+++ src/quorum/tests/test_price_feed_providers.py
@@ -1,7 +1,12 @@
 import json5 as json
 
 import quorum.tests.conftest as conftest
-from quorum.apis.price_feeds import ChainLinkAPI, CoinGeckoAPI, PriceFeedData
+from quorum.apis.price_feeds import (
+    ChainLinkAPI,
+    CoinGeckoAPI,
+    CoinMarketCapAPI,
+    PriceFeedData,
+)
 from quorum.utils.chain_enum import Chain
 
 EXPECTED_DIR = conftest.EXPECTED_DIR / "test_price_feed_providers"
@@ -33,3 +38,16 @@ def test_coingecko(tmp_cache):
         with open(file) as f:
             expected = PriceFeedData(**json.load(f))
         assert api.get_price_feed(Chain.ETH, file.stem) == expected
+
+
+def test_coinmarketcap():
+    api = CoinMarketCapAPI()
+    address = "0xc1Fa6E2E8667d9bE0Ca938a54c7E0285E9Df924a"
+    data = api.get_price_feed(Chain.ETH, address)
+    assert data.address.lower() == address.lower()
+    assert (
+        data.proxy_address.lower()
+        == "0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee".lower()
+    )
+    assert data.name == "Wrapped eETH"
+    assert data.pair == "weETH"
